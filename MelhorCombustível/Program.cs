d3d71895bb2c4387732fdf47a29fad0e827d7cbd
Augusto Omena <augusto.omena.n@gmail.com>
2023-01-25 14:27:25 -0300

    Adicionar arquivos de projeto.

---
+++ MelhorCombustível/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program {
+    public static void Main(String[] args)
+    {
+
+        double gasolina, alcool;
+        Console.WriteLine("Qual o Preço da gasolina?");
+        int v = Console.Read();
+        gasolina = v;
+        Console.WriteLine(gasolina);
+
+}
