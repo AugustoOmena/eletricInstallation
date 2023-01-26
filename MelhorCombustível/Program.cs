a1eab77f28d5ee6903feed87272ec5d9ea62b8dc
Augusto Omena <augusto.omena.n@gmail.com>
2023-01-26 11:10:26 -0300

    bestFuel

--- MelhorCombustível/Program.cs
+++ MelhorCombustível/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Globalization;
 
 class Program {
     public static void Main(String[] args)
     {
 
         double gasolina, alcool;
-        Console.WriteLine("Qual o Preço da gasolina?");
-        int v = Console.Read();
-        gasolina = v;
-        Console.WriteLine(gasolina);
-
+        Console.Write("Qual o Preço do álcool? ");
+        double va = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine();
+        alcool = va;
+        Console.Write("Qual o preço da Gasolina? ");
+        double vg = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        gasolina = vg;
+        double mc = (va / vg);
+        Console.WriteLine();
+        Console.WriteLine("A diferença foi de: " + mc.ToString("F2", CultureInfo.InvariantCulture));
+        try
+        {
+            if (mc > 0.7)
+            {
+                Console.WriteLine("vale a pena a gasolina. ");
+            }
+            else if (mc < 0.7)
+            {
+                Console.WriteLine("vale a pena o álcool.");
+            }
+            else
+            {
+                Console.WriteLine("tanto faz abastecer com um ou outro.");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Valor não correspondente.");
+        }
+    }
 }
