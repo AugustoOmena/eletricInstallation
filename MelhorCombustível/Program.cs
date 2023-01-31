8bde0d6528b88222632b2b7c26e2594ab44f9131
Augusto Omena <augusto.omena.n@gmail.com>
2023-01-31 16:36:26 -0300

    Adicionado exceção

--- MelhorCombustível/Program.cs
+++ MelhorCombustível/Program.cs
@@ -5,42 +5,56 @@ using System.Globalization;
 class Program {
     public static void Main(String[] args)
     {
-        int c = 0;
-        do
+        try
         {
-            double gasolina, alcool;
-            Console.Write("Qual o Preço do álcool? ");
-            double va = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine();
-            alcool = va;
-            Console.Write("Qual o preço da Gasolina? ");
-            double vg = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            gasolina = vg;
-            double mc = (va / vg);
-            Console.WriteLine();
-            Console.WriteLine("A diferença foi de: " + mc.ToString("F2", CultureInfo.InvariantCulture));
-            try
-            {
-                if (mc > 0.7)
-                {
-                    Console.WriteLine("vale a pena a gasolina. ");
-                }
-                else if (mc < 0.7)
-                {
-                    Console.WriteLine("vale a pena o álcool.");
-                }
-                else
-                {
-                    Console.WriteLine("tanto faz abastecer com um ou outro.");
-                }
-            }
-            catch
-            {
-                Console.WriteLine("Valor não correspondente.");
-            }
+            int c = 0;
 
-            Console.WriteLine("Quer calcular denovo? 1 = sim, 2 = não ");
-            c = int.Parse(Console.ReadLine());
-        } while (c == 1);
+            do
+            {
+                double gasolina, alcool;
+                Console.Write("Qual o Preço do álcool? ");
+                double va = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+                alcool = va;
+                Console.Write("Qual o preço da Gasolina? ");
+                double vg = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                gasolina = vg;
+                double mc = (va / vg);
+                Console.WriteLine();
+                Console.WriteLine("A diferença foi de: " + mc.ToString("F2", CultureInfo.InvariantCulture));
+                try
+                {
+                    if (mc > 0.7)
+                    {
+                        Console.WriteLine("vale a pena a gasolina. ");
+                    }
+                    else if (mc < 0.7)
+                    {
+                        Console.WriteLine("vale a pena o álcool.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("tanto faz abastecer com um ou outro.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Valor não correspondente.");
+                }
+                do
+                {
+                    Console.WriteLine("Quer calcular denovo? 1 = sim, 2 = não ");
+                    c = int.Parse(Console.ReadLine());
+                } while (c != 1 && c != 2);
+
+
+            } while (c == 1);
+        }
+        catch
+        {
+            Console.WriteLine("Erro inesperado! ");
+        }
+
+
     }
 }
